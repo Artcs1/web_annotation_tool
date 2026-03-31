b373548eb59efa6458c61818987bd86eca00052e
artcs1 <ju.jeffri.v@gmail.com>
2026-03-31 17:25:37 +0000

    update

--- app.py
+++ app.py
@@ -229,8 +229,8 @@ class VideoAnnotationApp:
             num_videos   = len(videos)
             num_blocks   = num_videos//self.number_of_clips
 
-            #print(len(ann_incompleted))
-            #print(len(ann_completed))
+            print(len(ann_incompleted))
+            print(len(ann_completed))
 
             if len(ann_incompleted) == 0:
 
@@ -630,6 +630,9 @@ class VideoAnnotationApp:
 
             if len(ann_incompleted) == 0:
                 possible_choices = [(block, f) for block in np.arange(num_blocks) for f in self.choices_annotatedframe]
+                possible_choices = [block for block in possible_choices if block[0] <12 or block[0] >=24] # TEMPORAL UNTIL PRANAV FINISH
+
+
                 global_counts = [self.finegrained_annotations_collection.count_documents({"globalIndex": int((p+1)*self.number_of_clips), "videoInfo.annotationFrame": f}) for p, f in possible_choices]
                 possible_choices = [p for p, c in zip(possible_choices, global_counts) if c < self.annotator_per_clip]  
                 possible_choices = list(set(possible_choices) - set(ann_completed))
@@ -640,7 +643,8 @@ class VideoAnnotationApp:
                 start_index = int(rand_number*self.number_of_clips)
 
 
-                for f in reversed(self.choices_annotatedframe):
+                #for f in reversed(self.choices_annotatedframe):
+                for f in self.choices_annotatedframe:
                     if (rand_number, f) in set(possible_choices):
                         annotatedFrame = f
                         break
